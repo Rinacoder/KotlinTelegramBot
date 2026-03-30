de5eef30ba602ea92079f3bfbcf41962f5550492
rina <iraledy@yandex.ru>
2026-03-30 21:57:19 +0300

    KTB-02 Создание первичной версии словаря

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
 package org.example
 
 import java.io.File
+import java.io.IOException
+
+const val FILE_NAME = "words.txt"
 
 fun main() {
-
-    val wordsFile:File = File("words.txt")
-    val wordsLines = wordsFile.readLines()
-    for(i in wordsLines){
-        println(i)
+    System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
+    val wordsFile: File = File(FILE_NAME)
+    try {
+        val wordsLines = wordsFile.readLines()
+        for (line in wordsLines) {
+            println(line)
+        }
+    } catch (e: IOException) {
+        println("Ошибка при работе с файлом: ${e.message}")
     }
 
 }
